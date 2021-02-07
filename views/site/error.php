461163bb135cfa5bf3e838d5bf1005a649288d3c
Doston Ismailov <dostonberuniy@gmail.com>
2021-02-08 01:39:36 +0500

    error page

--- views/site/error.php
+++ views/site/error.php
@@ -9,19 +9,22 @@ use yii\helpers\Html;
 
 $this->title = $name;
 ?>
-<div class="site-error">
-
-    <h1><?= Html::encode($this->title) ?></h1>
-
-    <div class="alert alert-danger">
-        <?= nl2br(Html::encode($message)) ?>
+<section class="row">
+    <div class="container">
+        <div class="col-12">
+            <div class="card">
+                <div class="card-content">
+                    <div class="card-body text-center bg-transparent miscellaneous">
+                        <h1 class="error-title mt-1"><?= Html::encode($this->title) ?></h1>
+<!--                        <a href="--><?//= bu() ?><!--" class="btn btn-primary round glow m-2 btn-lg">--><?//= Yii::t('app', 'BACK TO HOME') ?><!--</a>-->
+                        <?= Html::a(Yii::t('app', 'Back'), 'javascript: void(0);', ['class' => 'btn btn-primary round glow m-2 btn-lg', 'onclick'=>'window.history.back()', 'title' => 'Назад']) ?>
+                        <div class="text-center">
+                            <?= Html::img(bu('themes/frest/app-assets/images/pages/maintenance-2.png'), ['class' => 'img-fluid']) ?>
+                        </div>
+                        <p class="pb-2"><?= nl2br(Html::encode($message)) ?></p>
+                    </div>
+                </div>
+            </div>
+        </div>
     </div>
-
-    <p>
-        The above error occurred while the Web server was processing your request.
-    </p>
-    <p>
-        Please contact us if you think this is a server error. Thank you.
-    </p>
-
-</div>
+</section>
